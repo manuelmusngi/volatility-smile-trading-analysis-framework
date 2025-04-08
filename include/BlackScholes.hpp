af9da174bfa3a7ac48d8975a97a6aeb09d7447b3
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2025-04-08 17:55:43 -0400

    Create BlackScholes.hpp

---
+++ include/BlackScholes.hpp
@@ -0,0 +1,6 @@
+#ifndef BLACK_SCHOLES_HPP
+#define BLACK_SCHOLES_HPP
+
+double blackScholesPrice(double spot, double strike, double rate, double time, double vol, bool isCall);
+
+#endif // BLACK_SCHOLES_HPP
